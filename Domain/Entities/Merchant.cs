7ba84c2feea04736f3c094e4f4040ebba27af902
a-laasaid <159695027+a-laasaid@users.noreply.github.com>
2024-06-22 20:47:33 +0300

    create Employee and Merchant

---
+++ Domain/Entities/Merchant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Merchant
+    {
+        public string? StoreName { get; set; }
+        public int? GovernorateId { get; set; }
+       // public virtual Governorate? Governorate { get; set; }
+
+        public int? CityId { get; set; }
+        //public virtual City? City { get; set; }
+
+        public decimal CostperRefusedOrder { get; set; }
+
+        public int? RefusedOrderPercentage { get; set; }
+
+    }
+}
