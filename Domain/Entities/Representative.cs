c6e1d29249b40a18f0575821f7e641730f752b9f
SaadHany99 <159723036+SaadHany99@users.noreply.github.com>
2024-06-22 20:42:41 +0300

    Add AppUser And Representative

---
+++ Domain/Entities/Representative.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Representative
+    {
+        public string Governrate { get; set; }
+        public string DiscountType { get; set; }
+        public int CompanyPercetage { get; set; }
+    }
+}
