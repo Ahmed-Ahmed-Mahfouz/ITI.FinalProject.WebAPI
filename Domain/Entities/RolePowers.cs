537d641e8f9f39e3a94cfa1b1e17e3ed867b73d4
Eslam-Aboelyazeed <eslamaboelyazeed22@gmail.com>
2024-06-22 20:42:29 +0300

    Adding RolePowers entity and PowerTypes enum Commit

---
+++ Domain/Entities/RolePowers.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class RolePowers
+    {
+        [Key]
+        //[ForeignKey()]
+        public Guid RoleId { get; set; }
+        public PowerTypes Power { get; set; }
+    }
+
+}
