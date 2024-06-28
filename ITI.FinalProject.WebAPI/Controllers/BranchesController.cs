50caa77192ad92d2be3f28156197355e9233410d
abeerzahran <abeerzahran32@gmail.com>
2024-06-28 03:06:11 +0300

    adding city controller

---
+++ ITI.FinalProject.WebAPI/Controllers/BranchesController.cs
@@ -0,0 +1,65 @@
+﻿using Application.DTOs.DisplayDTOs;
+using Application.DTOs.InsertDTOs;
+using Application.DTOs.UpdateDTOs;
+using Application.Interfaces.ApplicationServices;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.FinalProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchesController : ControllerBase
+    {
+        IGenericService<Branch, BranchDisplayDTO, BranchInsertDTO, BranchUpdateDTO> branchServ;
+        public BranchesController(IGenericService<Branch, BranchDisplayDTO, BranchInsertDTO, BranchUpdateDTO> _branchServ)
+        {
+            branchServ = _branchServ;
+        }
+        [HttpGet]
+        public async Task<ActionResult> getAllBranches()
+        {
+            List<BranchDisplayDTO> branches = await branchServ.GetAllObjects();
+            return Ok(branches);
+
+        }
+        [HttpGet("id")]
+        public async Task<ActionResult> getById(int id)
+        {
+            BranchDisplayDTO? branch = await branchServ.GetObject(p=>p.id==id);
+            if (branch == null)
+                return NotFound();
+            return Ok(branch);
+
+        }
+        [HttpPost]
+        public  ActionResult addBranch(BranchInsertDTO branch)
+        {
+            if (branch == null)
+                return BadRequest();
+            var result = branchServ.InsertObject(branch);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+
+
+        }
+        [HttpDelete]
+        public async Task<ActionResult> deleteBranch(int id)
+        {
+
+            bool result =await branchServ.DeleteObject(id);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+
+
+        }
+
+    }
+}
---
+++ ITI.FinalProject.WebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,65 @@
+﻿using Application.DTOs.DisplayDTOs;
+using Application.DTOs.InsertDTOs;
+using Application.DTOs.UpdateDTOs;
+using Application.Interfaces.ApplicationServices;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.FinalProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        IGenericService<City, CityDisplayDTO, CityInsertDTO, CityUpdateDTO> CityServ;
+        public CitiesController(IGenericService<City, CityDisplayDTO, CityInsertDTO, CityUpdateDTO> _CityServ)
+        {
+            CityServ = _CityServ;
+        }
+        [HttpGet]
+        public async Task<ActionResult> getAllCityes()
+        {
+            List<CityDisplayDTO> Cityes = await CityServ.GetAllObjects();
+            return Ok(Cityes);
+
+        }
+        [HttpGet("id")]
+        public async Task<ActionResult> getById(int id)
+        {
+            CityDisplayDTO? City = await CityServ.GetObject(p=>p.id==id);
+            if (City == null)
+                return NotFound();
+            return Ok(City);
+
+        }
+        [HttpPost]
+        public  ActionResult addCity(CityInsertDTO City)
+        {
+            if (City == null)
+                return BadRequest();
+            var result = CityServ.InsertObject(City);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+
+
+        }
+        [HttpDelete]
+        public async Task<ActionResult> deleteCity(int id)
+        {
+
+            bool result =await CityServ.DeleteObject(id);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+
+
+        }
+
+    }
+}
