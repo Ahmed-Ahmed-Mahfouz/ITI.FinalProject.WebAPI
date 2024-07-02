423f0ae70bc07ab890286e7116cda899b3a9891b
a-laasaid <159695027+a-laasaid@users.noreply.github.com>
2024-07-02 05:28:12 +0300

    Employee & Merchant

--- ITI.FinalProject.WebAPI/Controllers/MerchantController.cs
+++ ITI.FinalProject.WebAPI/Controllers/MerchantController.cs
@@ -5,6 +5,7 @@ using Application.Interfaces;
 using Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.ComponentModel.DataAnnotations;
 
 namespace ITI.FinalProject.WebAPI.Controllers
@@ -22,6 +23,12 @@ namespace ITI.FinalProject.WebAPI.Controllers
             _logger = logger;
         }
 
+        [SwaggerOperation(
+        Summary = "This Endpoint returns a list of merchants",
+        Description = ""
+        )]
+        [SwaggerResponse(200, "Returns a list of merchants", Type = typeof(IEnumerable<MerchantResponseDto>))]
+        [SwaggerResponse(404, "There weren't any merchants in the database", Type = typeof(void))]
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -29,37 +36,27 @@ namespace ITI.FinalProject.WebAPI.Controllers
             return Ok(response?.ToList());
         }
 
+        [SwaggerOperation(
+        Summary = "This Endpoint returns the specified merchant",
+        Description = ""
+        )]
+        [SwaggerResponse(200, "Returns the specified merchant", Type = typeof(MerchantResponseDto))]
+        [SwaggerResponse(404, "The id that was given doesn't exist in the db", Type = typeof(void))]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id)
         {
-            MerchantResponseDto? response = await _MerchantService.GetObject(m=>m.Id == id);
+            MerchantResponseDto? response = await _MerchantService.GetObject(m => m.Id == id);
             if (response == null)
                 return NotFound();
             return Ok(response);
         }
 
-        //[HttpGet("paginated")]
-        //public async Task<ActionResult<PaginationResponse<MerchantResponseDto>>> GetMerchants([FromQuery] PaginationParameters paginationParameters)
-        //{
-        //    var Merchants = _MerchantService.GetMerchantsPaginated();
-        //    _logger.LogError("Merchants", Merchants);
-        //    int totalRecords = await Merchants.CountAsync();
-
-        //    List<MerchantResponseDto>? listOfTrsders = await Merchants
-        //        .Skip((paginationParameters.PageNumber - 1) * paginationParameters.PageSize)
-        //        .Take(paginationParameters.PageSize)
-        //        .ToListAsync();
-        //    PaginationResponse<MerchantResponseDto> result =
-        //        new PaginationResponse<MerchantResponseDto>()
-        //        {
-        //            Data = listOfTrsders,
-        //            PageNo = paginationParameters.PageNumber,
-        //            PageSize = paginationParameters.PageSize,
-        //            TotalRecords = totalRecords
-        //        };
-        //    return Ok(result);
-        //}
-
+        [SwaggerOperation(
+        Summary = "This Endpoint returns a list of merchants filtered by a search string",
+        Description = ""
+        )]
+        [SwaggerResponse(200, "Returns a list of filtered merchants", Type = typeof(IEnumerable<MerchantResponseDto>))]
+        [SwaggerResponse(400, "The search string is empty or null", Type = typeof(void))]
         [HttpGet("filtered")]
         public async Task<IActionResult> GetFilteredMerchants([FromQuery] string searchString)
         {
@@ -71,6 +68,12 @@ namespace ITI.FinalProject.WebAPI.Controllers
             return Ok(Merchants?.ToList());
         }
 
+        [SwaggerOperation(
+        Summary = "This Endpoint inserts a merchant element in the db",
+        Description = ""
+        )]
+        [SwaggerResponse(200, "Confirms that the merchant was inserted successfully", Type = typeof(MerchantAddDto))]
+        [SwaggerResponse(400, "Validation errors occurred while inserting the merchant", Type = typeof(void))]
         [HttpPost]
         public async Task<ActionResult> AddMerchant(MerchantAddDto MerchantAddDto)
         {
@@ -80,19 +83,31 @@ namespace ITI.FinalProject.WebAPI.Controllers
             return BadRequest(string.Join(", ", errors.Select(err => err.ErrorMessage)));
         }
 
+        [SwaggerOperation(
+        Summary = "This Endpoint updates the specified merchant",
+        Description = ""
+        )]
+        [SwaggerResponse(200, "Confirms that the merchant was updated successfully", Type = typeof(MerchantResponseDto))]
+        [SwaggerResponse(400, "Validation errors occurred while updating the merchant", Type = typeof(void))]
         [HttpPut("{MerchantId}")]
         public async Task<IActionResult> UpdateMerchant(string MerchantId, MerchantUpdateDto MerchantUpdateDto)
         {
             List<ValidationResult>? errors = await _MerchantService.UpdateObject(MerchantUpdateDto);
             if (errors?.Count == 0)
             {
-                MerchantResponseDto? updatedMerchant = await _MerchantService.GetObject(m=> m.Id==MerchantId);
+                MerchantResponseDto? updatedMerchant = await _MerchantService.GetObject(m => m.Id == MerchantId);
                 return Ok(updatedMerchant);
             }
             else
                 return BadRequest(string.Join(", ", errors.Select(err => err.ErrorMessage)));
         }
 
+        [SwaggerOperation(
+        Summary = "This Endpoint deletes the specified merchant from the db",
+        Description = ""
+        )]
+        [SwaggerResponse(204, "Confirms that the merchant was deleted successfully", Type = typeof(void))]
+        [SwaggerResponse(404, "The id that was given doesn't exist in the db", Type = typeof(void))]
         [HttpDelete("{MerchantId}")]
         public async Task<IActionResult> DeleteMerchant(string MerchantId)
         {
